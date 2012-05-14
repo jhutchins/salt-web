4265bd4e19619e594c26a2cd438406397e422422
Jeff Hutchins <jhutchins@getjive.com>
2012-05-13 23:42:28 -0600

    Template for something of interest.

---
+++ saltweb/static/app.js
@@ -0,0 +1,6 @@
+function MinionCtrl($scope){
+  $scope.minions = [
+    {name:'pbx1', status:'active', label:'success'},
+    {name:'pbx2', status:'inactive', label:'important'},
+    {name:'pbx3', status:'active', label:'success'}];
+}
